0d843e7468d5c459aefca9978cd1f2b48f2f27b8
Elias Franco Duarte <eliasfranco20160055@gmail.com>
2025-08-02 21:23:40 +0000

    Creando proyecto de hxhapi

---
+++ hxh_api/bin/hxh_api.dart
@@ -0,0 +1,5 @@
+import 'package:hxh_api/hxh_api.dart' as hxh_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hxh_api.calculate()}!');
+}
---
+++ hxh_api/test/hxh_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:hxh_api/hxh_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
